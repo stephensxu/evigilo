df618cc4a0e5302ab765095645f6de84edf76cdd
KensoDev <avi@kensodev.com>
2014-12-22 13:04:50 +0200

    No need for so much whitespace

--- evigilo.rb
+++ evigilo.rb
@@ -14,14 +14,12 @@ class Evigilo < Sinatra::Base
       changelog.data     = params[:data]
       changelog.snapshot = params[:snapshot]
     end
-
     { result: !changelog.new_record?, version: changelog.version }.to_json
   end
 
   get '/versions/:table_name/:id' do
     content_type :json
     versions = ChangeLog.where(object_name: params[:table_name], object_id: params[:id]).pluck(:version)
-
     { result: 'ok', versions: versions }.to_json
   end
 
@@ -35,6 +33,5 @@ class Evigilo < Sinatra::Base
     else
       halt 404, "Version was not found"
     end
-
   end
 end
